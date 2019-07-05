b2b27a4d4191fa35a701fb9ad811c206a39fcba9
vineela6005@gmail.com <vineela6005@gmail.com>
2019-07-05 23:19:10 +0530

    first commit

---
+++ src/com/mits/Sample.java
@@ -0,0 +1,10 @@
+package com.mits;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello Jenkins..");
+	}
+
+}
